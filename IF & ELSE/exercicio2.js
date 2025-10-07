4b188bac92a3b22c452eafa6a9245c80d187fb7b
Joyce Kelly <joycekellydasilva17@outlook.com>
2025-10-06 22:21:33 -0300

    --commit
    Exercicios de IF E ELSE

---
+++ IF & ELSE/exercicio2.js
@@ -0,0 +1,9 @@
+let idade = Number(prompt("Digite sua idade"))
+
+if(!idade){
+alert("Digite uma idade válida!")
+}else if (idade>=18){
+    alert("Você é Maior de idade!")
+}else{
+    alert("Você é Menor de idade!")
+}
